185a418b14546d21e4177417d1963c273f40ac86
julien <julien_90@hotmail.es>
2019-10-29 10:42:53 +0100

    StringUtils y commons-lang3

--- src/main/java/julien/programming/training/Application.java
+++ src/main/java/julien/programming/training/Application.java
@@ -2,10 +2,16 @@ package julien.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
 
+	public int countWords(String word) {
+		String[] separateWords = StringUtils.split(word, ' ');
+		return(separateWords == null)? 0 : separateWords.length;
+	}
+	
     public void greet(){
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
@@ -24,6 +30,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
-      app.greet();
+	    app.greet();
+	    int count = app.countWords("I have four words");
+	    System.out.println("Word count: " + count); 
     }
 }
